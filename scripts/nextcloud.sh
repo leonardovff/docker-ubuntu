9cf0ff734c13ee35f1b6f1eca23b8268610a691c
leonardo.ferreira <leonardovff@gmail.com>
2020-12-26 18:13:56 -0300

    fix nextcloud bash to production

--- scripts/nextcloud.sh
+++ scripts/nextcloud.sh
@@ -3,9 +3,9 @@ cd $(dirname "$0")
 cd ../apps/
 mkdir mariadb
 mkdir nextcloud
-# curl -LO https://download.nextcloud.com/server/releases/nextcloud-20.0.4.zip
-# unzip nextcloud-20.0.4.zip
-# rm nextcloud-20.0.4.zip
+curl -LO https://download.nextcloud.com/server/releases/nextcloud-20.0.4.zip
+unzip nextcloud-20.0.4.zip
+rm nextcloud-20.0.4.zip
 
 cd ..
 # up the containers
